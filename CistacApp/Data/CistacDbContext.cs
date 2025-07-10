5115ea7c34ddf9d0b94c8d812b7448fc7b135146
Burak Yaman <thisrweib@hotmail.com>
2025-07-10 18:18:10 +0300

    fix

--- CistacApp/Data/CistacDbContext.cs
+++ CistacApp/Data/CistacDbContext.cs
@@ -36,10 +36,7 @@ public partial class CistacDbContext : DbContext
 
     public virtual DbSet<Recommendation> Recommendations { get; set; }
 
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-    // => optionsBuilder.UseSqlServer("Server=cistac.database.windows.net;Database=CISTAC;User ID=cistac;Password=0Kuzen2025!_;TrustServerCertificate=True;");
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Booking>(entity =>
